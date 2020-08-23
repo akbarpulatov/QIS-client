a78d5d5b40e993b87c491aac8f39439083f54492
akbar <akbarpulatov7@gmail.com>
2020-08-24 01:14:05 +0500

    example ni ishlatib tushunib oldim http post

--- QIS_Client/QIS_Client.ino
+++ QIS_Client/QIS_Client.ino
@@ -17,7 +17,61 @@ uint8_t uidLength;
 #define PN532_RESET (3)
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
-void SetupTimer();
+// ======================================================================
+#include <EtherCard.h>
+
+// OAUTH key from http://arduino-tweet.appspot.com/
+#define TOKEN   "Insert-your-token-here"
+
+// ethernet interface mac address, must be unique on the LAN
+byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+const char website[] PROGMEM = "192.168.233.96";
+char tweet[100];
+//String tweet;
+static byte session;
+
+byte Ethernet::buffer[700];
+Stash stash;
+
+char UID[] = "5254454";
+char score[] = "2";
+
+static void sendToTwitter () {
+  Serial.println("Sending tweet...");
+  byte sd = stash.create();
+
+
+  
+  stash.println("{");
+  stash.print("\"UID\" : \"");
+  stash.print(UID);
+  stash.println("\",");
+  stash.print("\"Score\" : ");
+  stash.print(score);
+  stash.println("\"\r\n}");
+  
+  stash.save();
+  int stash_size = stash.size();
+
+
+  // Compose the http POST request, taking the headers below and appending
+  // previously created stash in the sd holder.
+  Stash::prepare(PSTR("POST / HTTP/1.0" "\r\n"
+    "Host: $F" "\r\n"
+    "Content-Length: $D" " "
+    "$n"
+    "\r\n"
+    "$H"),
+  website, website, stash_size, sd);
+
+  
+
+  // send the packet - this also releases all stash buffers once done
+  // Save the session ID so we can watch for it in the main loop.
+  session = ether.tcpSend();
+}
+// ======================================================================
 
 void SetupNFC() {
     nfc.begin();
--- http_post_with_Ethercard/http_post_with_Ethercard.ino
+++ http_post_with_Ethercard/http_post_with_Ethercard.ino
@@ -1,28 +1,22 @@
 #include <EtherCard.h>
-
-// OAUTH key from http://arduino-tweet.appspot.com/
-#define TOKEN   "Insert-your-token-here"
-
 // ethernet interface mac address, must be unique on the LAN
 byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
 const char website[] PROGMEM = "192.168.233.96";
-char tweet[100];
-//String tweet;
 static byte session;
-
 byte Ethernet::buffer[700];
 Stash stash;
 
 char UID[] = "5254454";
 char score[] = "2";
+uint32_t msTimer;
+uint32_t msTimer1;
+
 
 static void sendToTwitter () {
   Serial.println("Sending tweet...");
   byte sd = stash.create();
-
-
-  
+ 
   stash.println("{");
   stash.print("\"UID\" : \"");
   stash.print(UID);
@@ -34,7 +28,6 @@ static void sendToTwitter () {
   stash.save();
   int stash_size = stash.size();
 
-
   // Compose the http POST request, taking the headers below and appending
   // previously created stash in the sd holder.
   Stash::prepare(PSTR("POST / HTTP/1.0" "\r\n"
@@ -44,8 +37,6 @@ static void sendToTwitter () {
     "\r\n"
     "$H"),
   website, website, stash_size, sd);
-
-  
 
   // send the packet - this also releases all stash buffers once done
   // Save the session ID so we can watch for it in the main loop.
@@ -71,20 +62,40 @@ void setup () {
 
   ether.printIp("SRV: ", ether.hisip);
 
-  sendToTwitter();
+//  sendToTwitter();
+  
+  SetupTimer();
 }
 
 void loop () {
   ether.packetLoop(ether.packetReceive());
-
-  const char* reply = ether.tcpReply(session);
-//  if (reply != 0) {
-//    Serial.println("Got a response!");
-//    Serial.println(reply);
-//  }
-
   
+ if(!msTimer){
+  msTimer = 1000;
+  Serial.println("+");
+  sendToTwitter();
+ }
 
+}
 
-  
+//===============================================
+void SetupTimer() {
+  // инициализация Timer1
+    cli();  // отключить глобальные прерывания
+    TCCR1A = 0;   // установить регистры в 0
+    TCCR1B = 0;
+
+    OCR1A = 15624; // установка регистра совпадения
+
+    TCCR1B |= (1 << WGM12);  // включить CTC режим 
+    TCCR1B |= (1 << CS10); // Установить биты на коэффициент деления 1024
+//    TCCR1B |= (1 << CS12);
+
+    TIMSK1 |= (1 << OCIE1A);  // включить прерывание по совпадению таймера 
+    sei(); // включить глобальные прерывания
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  if(msTimer)msTimer--;
 }
