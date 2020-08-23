78e16dd05f77c4d2138f24d15119394b81fe9b9a
akbar <akbarpulatov7@gmail.com>
2020-08-24 00:28:14 +0500

    exampleni o'zimga kerakli qilib modifikatsiya qilib oldim endi osnovnoy programmaga qo'shaman

--- http_post_with_Ethercard/http_post_with_Ethercard.ino
+++ http_post_with_Ethercard/http_post_with_Ethercard.ino
@@ -7,32 +7,45 @@
 byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
 const char website[] PROGMEM = "192.168.233.96";
-
+char tweet[100];
+//String tweet;
 static byte session;
 
 byte Ethernet::buffer[700];
 Stash stash;
 
+char UID[] = "5254454";
+char score[] = "2";
+
 static void sendToTwitter () {
   Serial.println("Sending tweet...");
   byte sd = stash.create();
 
-  const char tweet[] = "@solarkennedy the test Twitter sketch works!";
-  stash.print("token=");
-  stash.print(TOKEN);
-  stash.print("&status=");
-  stash.println(tweet);
+
+  
+  stash.println("{");
+  stash.print("\"UID\" : \"");
+  stash.print(UID);
+  stash.println("\",");
+  stash.print("\"Score\" : ");
+  stash.print(score);
+  stash.println("\"\r\n}");
+  
   stash.save();
   int stash_size = stash.size();
 
+
   // Compose the http POST request, taking the headers below and appending
   // previously created stash in the sd holder.
-  Stash::prepare(PSTR("POST http://$F/update HTTP/1.0" "\r\n"
+  Stash::prepare(PSTR("POST / HTTP/1.0" "\r\n"
     "Host: $F" "\r\n"
-    "Content-Length: $D" "\r\n"
+    "Content-Length: $D" " "
+    "$n"
     "\r\n"
     "$H"),
   website, website, stash_size, sd);
+
+  
 
   // send the packet - this also releases all stash buffers once done
   // Save the session ID so we can watch for it in the main loop.
@@ -65,8 +78,13 @@ void loop () {
   ether.packetLoop(ether.packetReceive());
 
   const char* reply = ether.tcpReply(session);
-  if (reply != 0) {
-    Serial.println("Got a response!");
-    Serial.println(reply);
-  }
+//  if (reply != 0) {
+//    Serial.println("Got a response!");
+//    Serial.println(reply);
+//  }
+
+  
+
+
+  
 }
